8f1f8a05b9165dbb04555412d0ce20f890ca3d5c
Prutha <65603809+Prutha3022@users.noreply.github.com>
2021-09-16 16:32:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+export const  firebaseConfig = {
+    apiKey: "AIzaSyATMf6VRkm_bFQWSUji_7SZ8ijv82P13gM",
+    authDomain: "storytellingapp-9808e.firebaseapp.com",
+    projectId: "storytellingapp-9808e",
+    storageBucket: "storytellingapp-9808e.appspot.com",
+    messagingSenderId: "680801276664",
+    appId: "1:680801276664:web:93c2cf6e2f3041816e210d",
+    measurementId: "G-YW9JX3DC2W"
+  };
